f0b2f095f1144593ee07a813099ce3fbcfebb3c1
Tyson Tate <tyson@tysontate.com>
2014-05-20 09:36:28 -0700

    Add test for Encode()

--- aws_test.go
+++ aws_test.go
@@ -56,3 +56,23 @@ func TestUnmarshalError(t *testing.T) {
 		t.Fatalf("Expected %#v, but got %#v", exp, got)
 	}
 }
+
+func TestParamEncode(t *testing.T) {
+	tests := []struct {
+		key      string
+		value    string
+		expected string
+	}{
+		{"foo", "bar", "foo=bar"},
+		{"foo.1", "spaces here", "baz.1=spaces%20here"},
+		{"foo", "punctuation's", "foo=punctuation%27s"},
+	}
+
+	for i, test := range tests {
+		param := Param{Key: test.key, Val: test.value}
+		got := param.Encode()
+		if got != test.expected {
+			t.Errorf("test[%d]: expected %s, got %s", i, test.expected, got)
+		}
+	}
+}
